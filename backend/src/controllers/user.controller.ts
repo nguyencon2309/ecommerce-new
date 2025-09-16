23e6c05077143b1b9ddb3d8e2c088e8e9d312063
nguyencon2309 <117349138+nguyencon2309@users.noreply.github.com>
2025-09-16 07:15:12 +0700

    Update user.controller.ts

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -91,7 +91,7 @@ export const login = asyncHandler(async(req:Request,res:Response) => {
 
     res.cookie("token",token,{
         httpOnly:true,
-        secure:"false",
+        secure:false,
         sameSite:"lax",
         maxAge:7 * 24 * 60 * 60 * 1000
     })
